eb48a5f3ba3bddb6a092a36ceac96530c74d7a0c
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-16 16:51:09 +0200

    feat:bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,34 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.toReversed();
+console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+console.info(`%cNomi più lunghi di 5 caratteri`, `font-weight: bold;`)
+
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+
+if (teachers[i].length >= 5) {
+    
+    longNames.push(teachers[i])
+  }
+}
+console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+console.warn('Eliminiamo Ed')
+
+teachers.splice(teachers.indexOf('Ed'), 1)
+console.log(teachers)
+
+//Inserisci un nume da eliminare dalla lista
+
+/* let name = prompt('Inserisci un nome tra quelli della lista')
+
+teachers.splice(teachers.indexOf(name), 1)
+console.log(teachers) */
